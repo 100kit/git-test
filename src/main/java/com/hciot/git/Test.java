7ba7338669868d2d832b99ea8e426696dafb5463
100kit <100kit@qq.com>
2023-06-27 17:39:59 +0800

    1003

--- src/main/java/com/hciot/git/Test.java
+++ src/main/java/com/hciot/git/Test.java
@@ -5,5 +5,7 @@ public class Test
     public static void main(String[] args) {
         System.out.println("11111111111");
         System.err.println("222222");
+        System.out.println("333333333");
+        System.out.println("444");
     }
 }
